374720c10b8fa27c08d306d2bf3e51b0b5d869f5
kolirina <irinairin@list.ru>
2024-07-26 19:21:15 +0300

    feat: add tests for csx

--- src/utils/csx.tsx
+++ src/utils/csx.tsx
@@ -29,36 +29,3 @@ export const downloadCSV = (items: Animal[]) => {
   link.click();
   document.body.removeChild(link);
 };
-
-// import { Animal } from '../types';
-
-// export const downloadCSV = (items: Animal[]) => {
-//   const headers = ['UID', 'Name', 'Description'];
-//   const rows = items.map((item) => [
-//     item.uid,
-//     item.name,
-//     // item.descriptions?.join(', ') || '',
-//     const descriptions: string[] = [];
-//     if (item.earthAnimal) descriptions.push('Earth Animal');
-//     if (item.earthInsect) descriptions.push('Earth Insect');
-//     if (item.avian) descriptions.push('Avian');
-//     if (item.canine) descriptions.push('Canine');
-//     if (item.feline) descriptions.push('Feline');
-
-//     if (descriptions.length === 0) {
-//       descriptions.push('an infinitely cute animal');
-//     }
-//   ]);
-
-//   const csvContent =
-//     'data:text/csv;charset=utf-8,' +
-//     [headers, ...rows].map((e) => e.join(',')).join('\n');
-
-//   const encodedUri = encodeURI(csvContent);
-//   const link = document.createElement('a');
-//   link.setAttribute('href', encodedUri);
-//   link.setAttribute('download', `${items.length}_animals.csv`);
-//   document.body.appendChild(link);
-//   link.click();
-//   document.body.removeChild(link);
-// };
